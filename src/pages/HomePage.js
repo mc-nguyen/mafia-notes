3433c112180a2e99e4ff4c061a922540a26b57f6
MC Nguyen <marvin.calvin.1705@gmail.com>
2022-11-16 19:52:22 -0800

    Home Page design

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {auth, db} from "../database/firebase";
 import {doc, getDoc} from "firebase/firestore";
+import {Button, Card, CardActions, CardContent, CardMedia, Container, Typography} from "@mui/material";
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: "NONE"
+            name: "Moderator",
+            uid: null
         }
     }
 
@@ -15,11 +17,12 @@ class HomePage extends React.Component {
             unsub()
             if (user) {
                 const snap = await getDoc(doc(db, 'users', user.uid))
+                this.setState({uid: user.uid})
                 if (snap.exists()) {
                     console.log(snap.data())
                     this.setState(snap.data())
                 }
-            } else this.setState({name: "NONE"})
+            } else this.setState({name: "Moderator"})
         })
     }
 
@@ -29,7 +32,48 @@ class HomePage extends React.Component {
 
     render() {
         return (
-            <h1>Hello, {this.state.name}</h1>
+            <Card sx={{
+                maxWidth: 500,
+                minWidth: 200,
+                mt: 2,
+                mx: 'auto'
+            }}>
+                <CardMedia
+                    component='img'
+                    height='300'
+                    image='images/mafia.png'
+                    alt='Mafia Board Game wallpaper'
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="div">
+                        Hello, {this.state.name}
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary" align='justify'>
+                        There are {100000} visits and {100} games created. Thank you so
+                        much for visiting my page and choosing this application for
+                        playing Mafia and Werewolf! <br/>
+                        To play a new game, please <strong>press</strong> on the button below!
+                    </Typography>
+                </CardContent>
+                <CardActions sx={{ alignContent: 'right' }}>
+                    <Container component='div' align='right' sx={{ pb: 1 }}>
+                        <Button variant='contained' color='error'
+                                sx={{ mt: 1,
+                                    fontWeight: 'bold',
+                                    '&:hover': {
+                                        color: 'red',
+                                        backgroundColor: 'white'
+                                    }}}
+                        >{(this.state.uid === null) ? "LOG-IN/SIGN-UP" : 'Create a Game'}</Button>
+                    </Container>
+                </CardActions>
+                <CardMedia
+                    component='img'
+                    height='300'
+                    image='images/werewolf.png'
+                    alt='Werewolf Board Game wallpaper'
+                />
+            </Card>
         );
     }
 }
